0188d59cd2212cd3a65a1117c3b38299fe64e617
annazeit <anna.zeit@outlook.com>
2025-01-03 16:32:26 +0000

    drew a circle

--- blog_code/src/main.rs
+++ blog_code/src/main.rs
@@ -1,10 +1,11 @@
 use bevy::prelude::*;
+use bevy::color::palettes::basic::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, draw_circle)
+        .add_systems(Update, draw_player)
         .run();
 }
 
@@ -12,6 +13,7 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-fn draw_circle(mut gizmos: Gizmos,) {
-    gizmos.circle_2d(Vec2::new(0.0, 0.0), 100.0, Color::RED);
+fn draw_player(mut gizmos: Gizmos,) {
+    let size_radius = 20.0;
+    gizmos.circle_2d(Vec2::new(0.0, 0.0), size_radius, RED);
 }
